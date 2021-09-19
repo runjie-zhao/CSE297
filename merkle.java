389c4f1d61c27e9f3be3fe5b04fd210af3c59478
Runjie Zhao <ruz222@lehigh.edu>
2021-09-19 14:06:35 -0400

    reading file

--- merkle.java
+++ merkle.java
@@ -2,14 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class merkle{
-	public String address;
-	public String value;
+	public static ArrayList<String> address = new ArrayList<>();
+	public static ArrayList<Integer> value = new ArrayList<>();
 	
 	public static void main(String []args) {
-		System.out.println(Read("temp.txt"));
+		System.out.println(Read("xt.txt"));
+		System.out.println();
+		System.out.println("Elements are" + address);
+		System.out.println();
+		System.out.println("Elements integer are " + value);
 	}
 	
 	public static String Read(String filepath) {
+		//The file must have the correct txt format
+		if(filepath.length()<4 || filepath.substring(filepath.length()-5).equals(".txt")) {
+			System.out.println("Error: Input file does not have correct format");
+			System.exit(0);
+		}
+		//Read from file
 		File file = new File(filepath);
 		BufferedReader reader = null;
 		StringBuffer sbf = new StringBuffer();
@@ -17,7 +27,22 @@ public class merkle{
 	    	reader = new BufferedReader(new FileReader(file));
 		    String tempStr;
 		    while ((tempStr = reader.readLine()) != null) {
-		      sbf.append(tempStr);
+		    	//Test if each line is composed of two value
+		    	String []arr = tempStr.split("\\s+");
+		    	if(arr.length>=3) {
+		    		System.out.println("The format in .txt file is wrong. Each line should be consist of 1 address and 1 value.");
+		    		System.exit(0);
+		    	}
+		    	//Assign the value of arr[0] to address
+		    	address.add(arr[0]);
+		    	//Use try catch to test if the second value is an integer
+		    	try {
+		    		value.add(Integer.parseInt(arr[1]));
+		    	}catch (Exception e) {
+		    		System.out.println("The second value is not an integer");
+		    		System.exit(0);
+		    	}
+		    	sbf.append(tempStr);
 		    }
 		    reader.close();
 		    return sbf.toString();
