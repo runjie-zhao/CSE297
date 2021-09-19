985ef875f881441c3a15ade2349ac79297a5cadb
Runjie Zhao <ruz222@lehigh.edu>
2021-09-19 14:34:23 -0400

    add a new file which includes the merkle block

--- merkle.java
+++ merkle.java
@@ -2,17 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class merkle{
+	//Initialize three variables which are address, value and mkblock
 	public static ArrayList<String> address = new ArrayList<>();
 	public static ArrayList<Integer> value = new ArrayList<>();
+	public static ArrayList<merkleblock> mkblock = new ArrayList<>();
 	
 	public static void main(String []args) {
+		
 		System.out.println(Read("xt.txt"));
 		System.out.println();
 		System.out.println("Elements are" + address);
 		System.out.println();
 		System.out.println("Elements integer are " + value);
+		System.out.println();
+		//Iterate the mkblock and show the value
+		for(int i = 0; i < mkblock.size(); i++) {
+			System.out.println(mkblock.get(i).address + " " + mkblock.get(i).value);
+		}
 	}
 	
+	
+	//Read from txt file
 	public static String Read(String filepath) {
 		//The file must have the correct txt format
 		if(filepath.length()<4 || filepath.substring(filepath.length()-5).equals(".txt")) {
@@ -42,6 +52,8 @@ public class merkle{
 		    		System.out.println("The second value is not an integer");
 		    		System.exit(0);
 		    	}
+		    	//Create the merkle tree leaves and assign the value to it.
+		    	Assign(arr[0],Integer.parseInt(arr[1]));
 		    	sbf.append(tempStr);
 		    }
 		    reader.close();
@@ -62,4 +74,10 @@ public class merkle{
 		  return sbf.toString();
 	}
 	
+	
+	//Create a new merkleblock class and assign the address and value to it.
+	public static void Assign(String address, int value) {
+		merkleblock mk = new merkleblock(address, value);
+		mkblock.add(mk);
+	}
 }
---
+++ merkleblock.java
@@ -0,0 +1,13 @@
+public class merkleblock{
+	String address;
+	int value;
+	String hash;
+	public merkleblock() {
+		return;
+	}
+	
+	public merkleblock(String address, int value) {
+		this.address = address;
+		this.value = value;
+	}
+}
