ce204afc5e0684a6b41425495990a504aca6a435
Runjie Zhao <ruz222@lehigh.edu>
2021-10-17 18:02:55 -0400

    runjie

--- Cli.java
+++ Cli.java
@@ -175,76 +175,27 @@ public class Cli {
 					System.out.println("Please enter the useable input value");
 					System.exit(0);
 				}
-				String[] filepaths1 = input_string1.split("\\s+");
-				ArrayList<Block> blocks1 = new ArrayList<>();
-				for (String filepath : filepaths1) {
-					ArrayList<String> address = new ArrayList<>();
-					ArrayList<Integer> values = new ArrayList<>();
-					// The file must have the correct txt format
-					if (filepath.length() < 4 || filepath.substring(filepath.length() - 5).equals(".txt")) {
-						System.out.println("Error: Input file does not have correct format");
-						System.exit(0);
-					}
-					// Read from file
-					File file = new File(filepath);
-					BufferedReader reader;
-					try {
-						reader = new BufferedReader(new FileReader(file));
-						String tempStr;
-
-						while ((tempStr = reader.readLine()) != null) {
-							// Test if each line is composed of two value
-							String[] arr = tempStr.split("\\s+");
-							if (arr.length >= 3) {
-								System.out.println(
-										"The format in .txt file is wrong. Each line should be consist of 1 address and 1 value.");
-								System.exit(0);
-							}
-							// Assign the value of arr[0] to address
-							address.add(arr[0]);
-							// Use try catch to test if the second value is an integer
-							try {
-								values.add(Integer.parseInt(arr[1]));
-							} catch (Exception e) {
-								System.out.println("The second value is not an integer");
-								System.exit(0);
-							}
+				try {
+					ArrayList<Block> blocks1 = read_file(input_string1);
+					if(blocks.size()<2) {
+						for (Block block : blocks1) {
+							//System.out.println(block.printBlock(true));
+							System.out.println("Validation Result is " + block.validate());
 						}
-						reader.close();
-						Tree tree = new Tree(address, values);
-						Block block;
-						if (blocks1.size() == 0) {
-							block = new Block(tree, 0.5);
-						} else {
-							block = new Block(tree, blocks1.get(blocks1.size() - 1), 0.5);
-						}
-						blocks1.add(block);
-					} catch (IOException e) {
-						System.out.println("File Cannot Be Found");
-						System.exit(0);
+					}else {
+						Blockchain chain = new Blockchain(blocks1);
+						boolean value = chain.validate();
+						System.out.println("Validation Result is " + value);
 					}
+				}catch(Exception e) {
+					System.out.println("Validation Result is false");
 				}
-				for (Block block : blocks1) {
-					System.out.println(block.printBlock(true));
-					System.out.println("Validation Result " + block.validate());
-				}
-				/*File output1 = new File(filepaths1[0].substring(0, filepaths1[0].length() - 4) + ".block.out");
-				try (BufferedWriter ostream = new BufferedWriter(new FileWriter(output1))) {
-					for (int i = blocks.size()-1; i >= 0; i--) {
-						ostream.write(blocks.get(i).printBlock(true));
-					}
-					ostream.newLine();
-					ostream.close();
-				} catch (IOException e) {
-					System.out.println("File Cannot Be Written");
-						System.exit(0);
-				}*/
 			}else if(choice.equals("2")){
 				Blockchain chain = new Blockchain(blocks);
 				boolean value = chain.balance("1412421151e2b031cd46b0478ce20a7kono110d4");
 				ArrayList<String> arr = chain.get_Res();
 				for(int i = 0; i < arr.size(); i++) {
-					//System.out.println(arr.get(i));
+					System.out.println(arr.get(i));
 				}
 			}else if(choice.equals("3")) {
 				System.out.println("Please enter the txt files");
